1008f2957ea4c7f9248615cced8fd41f89c92914
MichaelBarta <michael.barta@aon.at>
2019-10-29 14:52:01 +0100

    patient class added

---
+++ C#_Application/PDMS/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace PDMS
+{
+    public class Patient
+    {
+        
+
+        int patientID;
+        string name, surname, medication, socialsecurity,date_of_birth;
+        bool sex;
+        float height, weight;
+
+
+
+        public Patient()
+        {
+        }
+    }
+}
