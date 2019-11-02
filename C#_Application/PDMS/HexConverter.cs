bb02ad8a4ec85e1e53f3f89c3d2211b465b442f1
StefBBE <44952014+StefBBE@users.noreply.github.com>
2019-11-02 19:36:52 +0100

    Added Class+Method for hex card ids

---
+++ C#_Application/PDMS/HexConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace PDMS
+{
+    class HexConverter
+    {
+        public string converter(string numberasstring)
+        {
+            BigInteger cinBI = new BigInteger();
+            cinBI = BigInteger.Parse(numberasstring);
+            string cinhex = cinBI.ToString("X");
+            return cinhex;
+        }
+    }
+}
