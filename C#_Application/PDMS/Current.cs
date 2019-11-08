b604ecf900131368c20f49c4471cbc0c3d9b4c83
Michael Barta <be17b030@technikum-wien.at>
2019-11-08 12:41:58 +0100

    merge conflict resolved

--- C#_Application/PDMS/Current.cs
+++ C#_Application/PDMS/Current.cs
@@ -14,14 +14,24 @@ namespace PDMS
 
         public int Role { get => role; set => role = value; }
         public int Hexcid { get => hexcid; set => hexcid = value; }
-        public void curpatid (int i) { curpat.PatientID = i; }
-        public void curpatname(string i) { curpat.Name = i; }
-        public void curpatsurname(string i) { curpat.Surname = i; }
-        public void curpatsocial(string i) { curpat.Socialsecurity = i; }
-        public void curpatdob(string i) { curpat.Date_of_birth = i; }
-        public void curpatsex(bool i) { curpat.Sex = i; }
-        public void curpathei(float i) { curpat.Height = i; }
-        public void curpatwei(float i) { curpat.Weight = i; }
+        public void curpatids (int i) { curpat.PatientID = i; }
+        public void curpatnames(string i) { curpat.Name = i; }
+        public void curpatsurnames(string i) { curpat.Surname = i; }
+        public void curpatsocials(string i) { curpat.Socialsecurity = i; }
+        public void curpatdobs(string i) { curpat.Date_of_birth = i; }
+        public void curpatsexs(bool i) { curpat.Sex = i; }
+        public void curpatheis(float i) { curpat.Height = i; }
+        public void curpatweis(float i) { curpat.Weight = i; }
+
+
+        public int curpatidg() { return curpat.PatientID; }
+        public string curpatnameg() { return curpat.Name; }
+        public string curpatsurnameg() {return curpat.Surname; }
+        public string curpatsocialg() { return curpat.Socialsecurity; }
+        public string curpatdobg() {return curpat.Date_of_birth; }
+        public bool curpatsexg() { return curpat.Sex; }
+        public float curpatheig() { return curpat.Height; }
+        public float curpatweig() { return curpat.Weight; }
 
     }
 }
